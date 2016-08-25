e6e1bf2f25acea712fff7f24fd2f650435112c48
Amal Shehu <amalshehu@gmail.com>
2016-08-25 12:57:13 +0530

    Hello world - Initial commit

---
+++ hello-world/hello_world.py
@@ -0,0 +1,12 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+name=raw_input("Enter your name or press ENTER key:")
+def hello(name):
+    if not len(name)==0 and name == isalpha():
+       message = 'Hello,' + name + '!'
+    return message
+
+    else :
+        return "Hello,World!"
