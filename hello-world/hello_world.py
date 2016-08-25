11555cf141083c1cab62d84eeef57c02f7d5fc0a
Amal Shehu <amalshehu@gmail.com>
2016-08-25 14:14:25 +0530

    4 tests  passed in 0.01 seconds

--- hello-world/hello_world.py
+++ hello-world/hello_world.py
@@ -1,13 +1,9 @@
 #
 # Skeleton file for the Python "Hello World" exercise.
 #
-
-name=raw_input("Enter your name or press ENTER key:")
-def hello(name):
-
-    if not len(name)==0 and name == isalpha():
-        message = 'Hello,' + name + '!'
+def hello(name=''):
+    if not name == '':
+        message = "Hello, " + name + "!"
         return message
-
-    else :
-        return "Hello,World!"
+    else:
+        return "Hello, World!"
