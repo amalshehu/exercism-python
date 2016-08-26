e55d29ff7557342d42251a66528c90e5fa56f93d
Amal Shehu <amalshehu@gmail.com>
2016-08-26 23:41:44 +0530

    clock class initiated

--- clock/clock.py
+++ clock/clock.py
@@ -1,12 +1,7 @@
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
 
-import datetime
 
-clock1 = datetime.time(1, 2)
-print(clock1)
-print('hour       :', clock1.hour)
-print('minute     :', clock1.minute)
-
-clock2 = datetime.time(1, 2)
-print(clock2)
-print('hour       :', clock2.hour)
-print('minute     :', clock2.minute)
+class Clock:
+    seconds = 0
