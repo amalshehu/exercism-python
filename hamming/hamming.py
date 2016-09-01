fd667884595def1d536ba325476fd6003eb48356
Amal Shehu <amalshehu@gmail.com>
2016-09-01 23:33:27 +0530

    Hamming solved

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,11 +1,19 @@
 #  File:       hamming.py
-#  Purpose:    program that can calculate the Hamming difference between two DNA strands.
+#  Purpose:    calculate the Hamming difference between two DNA strands.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 1st September 2016, 11:00 PM
 
 # Implementation notes
 
-'''The Hamming distance is only defined for sequences of equal length. This means
-that based on the definition, each language could deal with getting sequences
-of equal length differently.'''
+'''The Hamming distance is only defined for sequences of equal length.
+ This means that based on the definition, each language could deal with
+ getting sequences of equal length differently.'''
+
+
+def distance(dna1, dna2):
+    hamm = 0
+    for h1, h2 in zip(dna1, dna2):
+        if h1 != h2:
+            hamm += 1
+    return hamm
