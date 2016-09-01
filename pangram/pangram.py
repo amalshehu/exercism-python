3ca2971a62a1902db99a0b97aaf6664eee6b6f1d
Amal Shehu <amalshehu@gmail.com>
2016-09-01 18:10:47 +0530

    Partial output

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,26 +1,28 @@
 #  File:       pangram.py
 #  Purpose:     # Determine if a sentence is a pangram.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 1st September 2016, 02:15 PM
+
 '''
 Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan gramma,
 "every letter") is a sentence using every letter of the alphabet at least once.
-The best known English pangram is "The quick brown fox jumps over the lazy dog."
-The alphabet used is ASCII, and case insensitive, from 'a' to 'z'
+The best known English pangram is "The quick brown fox jumps over the
+lazy dog."The alphabet used is ASCII, and case insensitive, from 'a' to 'z'
 inclusively.
 '''
-#  Programmer: Amal Shehu
-#  Course:     Exercism
-#  Date:       Thursday 1st September 2016, 10:30 AM
+
 
 
 def pangram(string):
-    alpha = []
-    for i in range(ord('a'), ord('n')+1):
-        alpha.append((chr(i)))
-    string = string.lower()
-    if i in alpha == item in string:
-        print ("pangram")
-    else:
-        print ("no")
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    storeStr = ""
+    for x in string:
+        if x in alpha:
+            storeStr += x
+    for x in alpha:
+        if x not in string:
+            print (storeStr, alpha)
+            return False
 
-
-pangram("The quick brown fox jumps over the lazy dog.")
+    return True
