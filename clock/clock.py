a6cea858a2ec197adb1e3faeb52c3c757b3ef223
Amal Shehu <amalshehu@gmail.com>
2016-09-01 13:51:16 +0530

    AssertionError: <clock.Clock instance at 0x7fcea556dc68> --fixed

--- clock/clock.py
+++ clock/clock.py
@@ -9,26 +9,26 @@
 #  Date:       Thursday 1st September 2016, 10:30 AM
 
 
-class Clock():
-    
+class Clock:
+
     _objecx = []
 
-    def __new__(clk, hour, minute):
+    def __new__(cls, hour, minute):
         for objec in _objecx:
-            if clk.obj == objec:
+            if cls.obj == objec:
                 return objec
-        return clk.obj
+        return cls.obj
 
     def __init__(self, hour, minute):
 
-        self.hour = (hour + (minute / 60)) % 24
+        self.hour = (hour + (minute // 60)) % 24
         self.minute = minute % 60
 
     def __eq__(self, another):
-        return self.hour == another.minute and self.minute == another.minute
+        return self.hour == another.hour and self.minute == another.minute
 
     def __str__(self):
-        return "{0:0d}:{1:0d}".format(self.hour,self.minute)
+        return "{0:02d}:{1:02d}".format(self.hour, self.minute)
 
     def add(self, minute_x):
-        return Clock(self.hour,self.minute + minute_x)
+        return Clock(self.hour, self.minute + minute_x)
