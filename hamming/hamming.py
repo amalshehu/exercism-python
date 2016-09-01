674f104bb5a5605b1c89aef5969f80b3e0892e9d
Amal Shehu <amalshehu@gmail.com>
2016-09-01 23:20:05 +0530

    Implementation notes added

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,5 +1,11 @@
 #  File:       hamming.py
-#  Purpose:     # Hamming
+#  Purpose:    program that can calculate the Hamming difference between two DNA strands.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 1st September 2016, 11:00 PM
+
+# Implementation notes
+
+'''The Hamming distance is only defined for sequences of equal length. This means
+that based on the definition, each language could deal with getting sequences
+of equal length differently.'''
