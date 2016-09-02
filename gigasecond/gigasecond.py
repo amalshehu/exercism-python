b8e4f088262f621540dd1fa171530977e8c4c3b2
Amal Shehu <amalshehu@gmail.com>
2016-09-02 15:21:52 +0530

    Program to calculate Gigaseconds

---
+++ gigasecond/gigasecond.py
@@ -0,0 +1,18 @@
+#  File:       gigasecond.py
+#  Purpose:    Program that calculates the moment when someone has lived for 10^9 seconds.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 1st September 2016, 03:15 PM
+
+def gigasecond(year,m,d):
+    sec = 1
+    mins = 60 * sec
+    hours = 60 * mins
+    day = 24 * hours
+    month = 31 * day
+    yr = 12 * month
+
+    totalSec =
+    print (gigSec)
+
+gigasecond()
