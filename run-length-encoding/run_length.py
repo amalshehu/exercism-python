ceaaea2d969214d93bd28602ca1691fd2cf4a51d
Amal Shehu <amalshehu@gmail.com>
2016-09-02 23:02:33 +0530

    Regex for encoding and decoding created

--- run-length-encoding/run_length.py
+++ run-length-encoding/run_length.py
@@ -3,3 +3,13 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 2nd September 2016, 09:10 PM
+
+from re import sub
+
+
+def encode(text):
+    return sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), text)
+
+
+def decode(text):
+    return sub(r'(\d+)(\D)', lambda m: m.group(2) * int(m.group(1)), text)
