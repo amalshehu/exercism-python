c749c107ce7b70cbc97d2d500c21f0fdf0688099
Amal Shehu <amalshehu@gmail.com>
2016-09-02 17:23:15 +0530

    Timedelta Added

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,18 +1,16 @@
 #  File:       gigasecond.py
-#  Purpose:    Program that calculates the moment when someone has lived for 10^9 seconds.
+#  Purpose:    Calculate date that someone will celebrate their 1Gsanniversary.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
-#  Date:       Thursday 1st September 2016, 03:15 PM
+#  Date:       Thursday 2nd September 2016, 05:00 PM
 
-def gigasecond(year,m,d):
-    sec = 1
-    mins = 60 * sec
-    hours = 60 * mins
-    day = 24 * hours
-    month = 31 * day
-    yr = 12 * month
+from datetime import timedelta, datetime
 
-    totalSec =
-    print (gigSec)
 
-gigasecond()
+def add_gigasecond(date):
+
+# A timedelta object represents a duration, the difference between two dates or times.
+    delta = timedelta(seconds=10**9)
+    print (date + delta)
+
+add_gigasecond(datetime(2016, 9, 2))
