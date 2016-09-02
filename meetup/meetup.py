e63adac77a9e1d42977d3b1d89a315440d0d965b
Amal Shehu <amalshehu@gmail.com>
2016-09-02 23:55:01 +0530

    Meetup functon initialized with week data dictionary

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -3,3 +3,12 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 2nd September 2016, 11:20 PM
+
+import calendar
+from datetime import date
+
+
+def meetup_day(m_Year, m_Month, weekDay, specWeekday):
+    lastDay = calendar.monthrange(year, month)[1]
+    print(lastDay)
+    weekDay_dict = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
