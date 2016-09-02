cf614abda71ca7a8aedd70edfc7c7ea4fefc99df
Amal Shehu <amalshehu@gmail.com>
2016-09-02 21:23:22 +0530

    Implement run-length encoding and decoding.

---
+++ run-length-encoding/run_length.py
@@ -0,0 +1,5 @@
+#  File:       run_length.py
+#  Purpose:    Implement run-length encoding and decoding.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 2nd September 2016, 09:10 PM
