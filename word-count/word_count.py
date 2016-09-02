33f6142e0efcc7d5dcd4657022dd339d524aea39
Amal Shehu <amalshehu@gmail.com>
2016-09-02 11:14:22 +0530

    Initial Commit

---
+++ word-count/word_count.py
@@ -0,0 +1,8 @@
+#  File:       word_count.py
+#  Purpose:    Write a program that given a phrase can count the occurrences of each word in that phrase.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Friday 2nd September 2016, 10:15 AM
+
+def word_count():
+    
