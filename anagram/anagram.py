2f4e1bdd3bb78deffd4bd2b8c14c00902cd68fe3
Amal Shehu <amalshehu@gmail.com>
2016-09-03 16:11:57 +0530

    Variable name shortened/0 PEP Error |[No issues]

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -5,11 +5,11 @@
 #  Date:       Saturday 3rd September 2016, 02:45 PM
 
 
-def detect_anagrams(string_1, string_2):
-    string_sort = sorted(string_1.lower())
+def detect_anagrams(str_1, string_2):
+    str_sort = sorted(str_1.lower())
     gram_list = []
     for item in string_2:
-        if sorted(item.lower()) == string_sort and item.lower() != string_1.lower():
+        if sorted(item.lower()) == str_sort and item.lower() != str_1.lower():
             gram_list.append(item)
 
     return gram_list
