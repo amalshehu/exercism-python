11ec9018dbe7f787f99975d9a006ffc5b22b6ef6
Amal Shehu <amalshehu@gmail.com>
2016-09-03 22:49:29 +0530

    Initial commit

---
+++ series/series.py
@@ -0,0 +1,6 @@
+#  File:       series.py
+#  Purpose:    Write a program that will take a string of digits and give you
+#              all the contiguous substrings of length `n` in that string.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Saturday 3rd September 2016, 10:50 PM
