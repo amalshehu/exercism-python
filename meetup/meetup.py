c76b049cdd74a9f95bc29e8234e23ee9e85d5125
Amal Shehu <amalshehu@gmail.com>
2016-09-03 13:39:37 +0530

    [No issues ]convert indent to space / reformat files based on PEP8

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -10,13 +10,21 @@ from datetime import date
 dayNames = [day for day in calendar.day_name]
 
 
-def meetup_day(m_Year, m_Month, weekDay, specWeekday):
-    dayIndex =  dayNames.index(weekDay)
-    chanceDate = [week[dayIndex]]
-    for week in calendar.monthcalendar(m_Year, m_Month)
-    if week[dayIndex]]
+def meetup_day(m_Year, m_Month, weekday, SPD):
+    dayStart = dayNames.index(weekday)
+    chanceDate = [
+        weekItem[dayStart]
+        for weekItem in calendar.monthcalendar(m_Year, m_Month)
+        if weekItem[dayStart]]
 
-    if specWeekday == 'teenth':
-        for dayNum in chanceDate:
-            if 13<= dayNum <= 19:
-                return date(m_Year, m_Month, dayNum)
+    if SPD == 'teenth':
+        for dayItem in chanceDate:
+            if 13 <= dayItem <= 19:
+                return date(m_Year, m_Month, dayItem)
+    elif SPD == 'last':
+        dayStart = -1
+    elif SPD == 'first':
+        dayStart = 0
+    else:
+        dayStart = int(SPD[0]) - 1
+    return date(m_Year, m_Month, chanceDate[dayStart])
