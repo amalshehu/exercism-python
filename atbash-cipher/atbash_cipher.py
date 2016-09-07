8786cfc2bf9c50a8736d1e9b1f8f057ad83d79d2
Amal Shehu <amalshehu@gmail.com>
2016-09-07 12:37:29 +0530

    Initial commit

---
+++ atbash-cipher/atbash_cipher.py
@@ -0,0 +1,9 @@
+#  File:       atbash_cipher.py
+#  Purpose:    Create an implementation of the atbash cipher, an ancient encryption system created in the Middle East.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 3rd September 2016, 09:15 PM
+
+
+def atbash_cipher():
+    
