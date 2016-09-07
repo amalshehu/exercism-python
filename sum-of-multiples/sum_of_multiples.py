b87d481ff7062ae1b2a8f0481fb28850a9c8cf9e
Amal Shehu <amalshehu@gmail.com>
2016-09-07 23:47:12 +0530

    TypeError: unsupported operand

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -4,3 +4,7 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Wednesday 7th September 2016, 11:00 PM
+
+
+def sum_of_multiples(limit, *div_list):
+    return sum((i for i in xrange(limit) if 0 in (i % d for d in div_list)))
