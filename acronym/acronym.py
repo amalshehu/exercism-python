2d97d0a1442a87773fe4cfc84e5ccac2075e166d
Amal Shehu <amalshehu@gmail.com>
2016-09-07 23:52:11 +0530

    Convert a long phrase to its acronym

---
+++ acronym/acronym.py
@@ -0,0 +1,5 @@
+#  File:       sum_of_multiples.py
+#  Purpose:    Convert a long phrase to its acronym
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Wednesday 7th September 2016, 11:50 PM
