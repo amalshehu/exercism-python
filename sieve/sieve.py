74c9879a1413aa87327dee080301da2701270b31
Amal Shehu <amalshehu@gmail.com>
2016-09-07 11:01:14 +0530

    Initial commit

---
+++ sieve/sieve.py
@@ -0,0 +1,5 @@
+#  File:       sieve.py
+#  Purpose:    Write a program that uses the Sieve of Eratosthenes to find all the primes from 2 up to a given number.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 3rd September 2016, 09:15 PM
