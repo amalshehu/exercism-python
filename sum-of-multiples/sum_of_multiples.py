55f48ee8383c1c6b9cf1155be3bb1374f21541b0
Amal Shehu <amalshehu@gmail.com>
2016-09-07 23:09:37 +0530

    Initial commit

---
+++ sum-of-multiples/sum_of_multiples.py
@@ -0,0 +1,6 @@
+#  File:       sum_of_multiples.py
+#  Purpose:    Write a program that, given a number, can find the sum of all
+#              the multiples of particular numbers up to but not including that number.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Wednesday 7th September 2016, 11:00 PM
