776e4020cf4a79b7db1d5be39084f8d021095aa5
Amal Shehu <amalshehu@gmail.com>
2016-09-07 14:36:53 +0530

    Decode funtionality aded

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -5,12 +5,20 @@
 #  Date:       Thursday 3rd September 2016, 09:15 PM
 
 
-def atbash_cipher(string):
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    rev = list(alpha)[::-1]
-    store = dict(zip(alpha, rev))
-    print (store)
+alpha = "abcdefghijklmnopqrstuvwxyz"
+rev = list(alpha)[::-1]
+store = dict(zip(alpha, rev))
 
 
+def decode(string):
+    dec = ''
+    for x in string:
+        dec += str(store[x])
+    return dec
 
-atbash_cipher("abcd")
+
+def encode(string):
+    enc = ''
+    for x in string:
+        enc += str(store.keys[x])
+    return enc
