3864feaff3a7810f43d65f1b8ab4bb401866bb5a
Amal Shehu <amalshehu@gmail.com>
2016-09-07 10:46:10 +0530

    Movement functons initialized

---
+++ robot-simulator/robot_simulator.py
@@ -0,0 +1,27 @@
+#  File:       robot_simulator.py
+#  Purpose:    A robot factory's test facility needs a program to verify robot movements.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 3rd September 2016, 05:00 PM
+
+.class Robot(object):
+    """docstring for Robot."""
+    def __init__(self,movement, x, y):
+
+        self.movement = movement
+        self.x = x_axis = 0
+        self.y = y_axis = 0
+
+    def advance(Robot):
+        x_axis += 1
+
+    def advance_twice(Robot):
+        x_axis += 2
+
+    def left(Robot):
+        x_axis -= 1
+
+    def right(Robot):
+        x_axis += 1
+
+    
