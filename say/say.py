75d2325f285ac4ae53f209113440cf3bf3ebce31
Amal Shehu <amalshehu@gmail.com>
2016-09-08 22:02:05 +0530

    Initial commit

---
+++ say/say.py
@@ -0,0 +1,5 @@
+#  File:       say.py
+#  Purpose:    Write a program that will take a number from 0 to 999,999,999,999 and spell out that number in English.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Wednesday 8th September 2016, 10:00 PM
