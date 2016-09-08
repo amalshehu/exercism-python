018d259d3f69e79a3ff2d6edaa9a9f1ce265c3a6
Amal Shehu <amalshehu@gmail.com>
2016-09-08 20:46:30 +0530

    Assertion Error : Hypertext Preprocessor

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,5 +1,12 @@
-#  File:       sum_of_multiples.py
+#  File:       acronym.py
 #  Purpose:    Convert a long phrase to its acronym
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Wednesday 7th September 2016, 11:50 PM
+
+
+def abbreviate(words):
+    out = ""
+    for word in words.upper().split():
+        out += (word[0])
+    print (out)
