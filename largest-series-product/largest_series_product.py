5d533d1cbb558ef6377979d50dd0e34eb8549b52
Amal Shehu <amalshehu@gmail.com>
2016-09-09 20:00:35 +0530

    Initial commit

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,5 @@
+#  File:       largest_series_product.py
+#  Purpose:    Calculate the largest product for a contiguous substring of digits of length n.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 9th September 2016, 08:02 PM
