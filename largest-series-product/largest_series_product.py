d56a7be2e44f213e7a87cc203e48e42f99e7f537
Amal Shehu <amalshehu@gmail.com>
2016-09-10 22:00:55 +0530

    Largest product for a contiguous substring of digits :Failed

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -3,3 +3,20 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 9th September 2016, 08:02 PM
+
+import re
+
+
+def largest_product(num, size):
+    multi = [reduce(lambda x, y: x * y, i, 1) for i in num_filter(num, size)]
+    return max(multi)
+
+
+def num_filter(num, size):
+    if len(num) > size or num == re.match(r'^[0-9]*$'), num):
+        raise ValueError
+    return single(map(int, list(num)), size)
+
+
+def single(x, size):
+    return [x[i: i + size] for i in range(len(x) - size + 1)]
