e3200bb5ba3da554151fcfc55f35858863b7a066
Amal Shehu <amalshehu@gmail.com>
2016-09-10 23:05:56 +0530

    Initial commit

---
+++ kindergarten-garden/kindergarten_garden.py
@@ -0,0 +1,5 @@
+#  File:       kindergarten_garden.py
+#  Purpose:    Write a program that, given a diagram, can tell you which plants each child in the kindergarten class is responsible for.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 10th September 2016, 10:51 PM
