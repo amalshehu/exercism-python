f4915347f0e2c23f80e2c15fdc91294c731b0387
Amal Shehu <amalshehu@gmail.com>
2016-09-12 03:04:13 +0530

    Add plant methord

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -7,8 +7,12 @@
 
 class Garden(object):
     plant_names = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
-    def __init__(self, blueprint,child):
-         self.child=("Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry"
-         self.child=sorted(child.split())
-         self.cup_row=blueprint.split()
-    
+    def __init__(self, blueprint, child):
+         self.child = "Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry"
+         self.child = sorted(child.split())
+         self.cup_row = blueprint.split()
+    def plant(self, child):
+        self.cup_start = self.child.index(child) * 2
+        cup = slice(cup_start,cup_start + 2)
+        for item in (cup_row[0][cup] + cup_row[1][cup])
+        return self.plant_names[item]
