a3426373c1ac7f1f6546d7ec1165bc7087545360
Amal Shehu <amalshehu@gmail.com>
2016-09-12 04:04:47 +0530

    Change variable names according to testMethord

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,18 +1,21 @@
 #  File:       kindergarten_garden.py
-#  Purpose:    Write a program that, given a diagram, can tell you which plants each child in the kindergarten class is responsible for.
+#  Purpose:    Write a program that, given a diagram, can tell you which plants each students in the kindergarten class is responsible for.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 10th September 2016, 10:51 PM
 
 
 class Garden(object):
-    plant_names = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
-    def __init__(self, blueprint, child):
-         self.child = "Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry"
-         self.child = sorted(child.split())
-         self.cup_row = blueprint.split()
-    def plant(self, child):
-        self.cup_start = self.child.index(child) * 2
-        cup = slice(cup_start,cup_start + 2)
-        for item in (cup_row[0][cup] + cup_row[1][cup])
-        return self.plant_names[item]
+    child_names = ["Alice", "Bob", "Charlie", "David",
+            "Eve", "Fred", "Ginny", "Harriet",
+            "Ileana", "Joseph", "Kincaid", "Larry"]
+    plant_names = {"C": "Clover", "G": "Grass",
+                   "R": "Radishes", "V": "Violets"}
+    def __init__(self, blueprint, students=child_names):
+        self.students = sorted(students)
+        self.cup_row = blueprint.split()
+    def plants(self, students):
+        cup_start = self.students.index(students) * 2
+        cup = slice(cup_start, cup_start + 2)
+        plant = [self.plant_names[item] for item in (self.cup_row[0][cup] + self.cup_row[1][cup])]
+        return plant
