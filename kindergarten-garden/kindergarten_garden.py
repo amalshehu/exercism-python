7c8bf75d3b3053673031fbd68e91ff5ef02cf210
Amal Shehu <amalshehu@gmail.com>
2016-09-12 04:11:37 +0530

    [No issues ]convert indent to space / reformat files based on PEP8

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,5 +1,6 @@
 #  File:       kindergarten_garden.py
-#  Purpose:    Write a program that, given a diagram, can tell you which plants each students in the kindergarten class is responsible for.
+#  Purpose:    Write a program that, given a diagram, can tell you which plants
+#              each students in the kindergarten class is responsible for.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 10th September 2016, 10:51 PM
@@ -7,15 +8,18 @@
 
 class Garden(object):
     child_names = ["Alice", "Bob", "Charlie", "David",
-            "Eve", "Fred", "Ginny", "Harriet",
-            "Ileana", "Joseph", "Kincaid", "Larry"]
+                   "Eve", "Fred", "Ginny", "Harriet",
+                   "Ileana", "Joseph", "Kincaid", "Larry"]
     plant_names = {"C": "Clover", "G": "Grass",
                    "R": "Radishes", "V": "Violets"}
+
     def __init__(self, blueprint, students=child_names):
         self.students = sorted(students)
         self.cup_row = blueprint.split()
+
     def plants(self, students):
         cup_start = self.students.index(students) * 2
         cup = slice(cup_start, cup_start + 2)
-        plant = [self.plant_names[item] for item in (self.cup_row[0][cup] + self.cup_row[1][cup])]
+        test_cup = (self.cup_row[0][cup] + self.cup_row[1][cup])
+        plant = [self.plant_names[item] for item in test_cup]
         return plant
