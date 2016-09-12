d8eda8a4764d87d711786ab0c51b1db0746868f3
Amal Shehu <amalshehu@gmail.com>
2016-09-12 23:10:36 +0530

    Initial commit

---
+++ grade-school/grade_school.py
@@ -0,0 +1,5 @@
+#  File:       grade_school.py
+#  Purpose:    Write a small archiving program that stores students' names along with the grade that they are in.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Monday 12th September 2016, 11:00 PM
