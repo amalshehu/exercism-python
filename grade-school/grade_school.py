46244113fc412799a0f04dcf3b11ec2cc43d2d60
Amal Shehu <amalshehu@gmail.com>
2016-09-13 00:44:22 +0530

    Initialize school class

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -3,3 +3,13 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Monday 12th September 2016, 11:00 PM
+
+class School(object):
+    """docstring for School."""
+    def __init__(self, school, grade, student):
+        self.school = school
+        self.grade = grade
+        self.student = student
+        self.students = students[]
+
+    
