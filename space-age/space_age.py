f3cb5cae9d57ca7cec7a73a208cae588e6bfbdce
Amal Shehu <amalshehu@gmail.com>
2016-09-17 18:10:34 +0530

    Initial commit

---
+++ space-age/space_age.py
@@ -0,0 +1,6 @@
+#  File:       space_age.py
+#  Purpose:    Write a program that, given an age in seconds, calculates
+#              how old someone is in terms of a given planet's solar years.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Saturday 17 September 2016, 06:09 PM
