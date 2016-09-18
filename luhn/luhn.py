a48818d9b0765b6aa0c0e0f15139315842251d63
Amal Shehu <amalshehu@gmail.com>
2016-09-19 00:28:31 +0530

    Reformat Luhn + Add anonymous function

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -6,16 +6,10 @@
 #  Date:       Sunday 18 September 2016, 09:55 PM
 
 
-def Luhn(card_number):
-    def digits_of(n):
-        return [int(d) for d in str(n)]
-    digits = digits_of(card_number)
-    odd = digits[-1::-2]
-    even = digits[-2::-2]
-    checksum = 0
-    checksum += sum(odd_digits)
-    for d in even_digits:
-        checksum += sum(digits_of(d*2))
-    return checksum % 10
-def checksum(card_number):
-    return luhn(card_number)
+class Luhn(object):
+    def __init__(self, card_number):
+        self.card_number = card_number
+
+    def addends(self):
+        luhn_function = lambda x: (2 * x - 9) if (x > 4) else (2 * x)
+        
