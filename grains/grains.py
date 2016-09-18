94d2fb9241874d7feb89aa6fee6bc14b76e3a441
Amal Shehu <amalshehu@gmail.com>
2016-09-18 19:00:10 +0530

    Add two lists with square and grain numbers

--- grains/grains.py
+++ grains/grains.py
@@ -4,3 +4,10 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Sunday 18 September 2016, 05:25 PM
+
+board = [x for x in range(1, 65)]
+grains = [x*2 for x in range(1, 65)]
+
+def on_square():
+    for x in range(1, 65):
+        board.append(x)
