d2e9201e5553088c3687eb48416a468788b28237
Amal Shehu <amalshehu@gmail.com>
2016-09-18 17:27:34 +0530

    Initial commit

---
+++ grains/grains.py
@@ -0,0 +1,6 @@
+#  File:       grains.py
+#  Purpose:    Write a program that calculates the number of grains of wheat
+#              on a chessboard given that the number on each square doubles.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Sunday 18 September 2016, 05:25 PM
