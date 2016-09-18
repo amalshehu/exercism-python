4a341a71e70e1ad6d2ce2ea18471b440eeb25f97
Amal Shehu <amalshehu@gmail.com>
2016-09-18 16:59:17 +0530

    Change variable name as per test methord

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -8,29 +8,29 @@
 
 class SpaceAge(object):
     def __init__(self, age):
-        self._seconds = float(age)
+        self.seconds = float(age)
         self._earth_year = 31557600
 
     def on_mercury(self):
-        return round(self._seconds / self._earth_year / 0.2408467, 2)
+        return round(self.seconds / self._earth_year / 0.2408467, 2)
 
     def on_venus(self):
-        return round(self._seconds / self._earth_year / 0.61519726, 2)
+        return round(self.seconds / self._earth_year / 0.61519726, 2)
 
     def on_earth(self):
-        return round(self._seconds / self._earth_year, 2)
+        return round(self.seconds / self._earth_year, 2)
 
     def on_mars(self):
-        return round(self._seconds / self._earth_year / 1.8808158, 2)
+        return round(self.seconds / self._earth_year / 1.8808158, 2)
 
     def on_jupiter(self):
-        return round(self._seconds / self._earth_year / 11.862615, 2)
+        return round(self.seconds / self._earth_year / 11.862615, 2)
 
     def on_saturn(self):
-        return round(self._seconds / self._earth_year / 29.447498, 2)
+        return round(self.seconds / self._earth_year / 29.447498, 2)
 
     def on_uranus(self):
-        return round(self._seconds / self._earth_year / 84.016846, 2)
+        return round(self.seconds / self._earth_year / 84.016846, 2)
 
     def on_neptune(self):
-        return round(self._seconds / self._earth_year / 164.79132, 2)
+        return round(self.seconds / self._earth_year / 164.79132, 2)
