089b47477863f02882c3f07261961b63d4c3f347
Amal Shehu <amalshehu@gmail.com>
2016-09-18 15:17:26 +0530

    Add earth age function

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -4,3 +4,19 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Saturday 17 September 2016, 06:09 PM
+
+
+class SpaceAge(object):
+    """docstring for SpaceAge."""
+    def __init__(self, _seconds):
+        self._seconds = _seconds
+
+    def on_earth(self):
+        return round((self._seconds / 31557600), 2)
+
+    def on_mercury(self):
+        return round((self._seconds / 31557600) * 0.240846, 2)
+
+obj = SpaceAge(1e6)
+print (obj.on_earth())
+print (obj.on_mercury())
