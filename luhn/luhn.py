6c9016a9987bf2efcfd8f237f8bbfbd5c4328ba1
Amal Shehu <amalshehu@gmail.com>
2016-09-18 23:35:10 +0530

    Add card verification function

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -4,3 +4,11 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Sunday 18 September 2016, 09:55 PM
+
+
+def Luhn(card_number):
+
+    digits = digits_of(card_number)
+    
+def checksum(card_number):
+    return luhn(card_number)
