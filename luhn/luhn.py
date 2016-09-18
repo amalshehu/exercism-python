ba3d02f1118ce752185f70c86c6cd961a6e93840
Amal Shehu <amalshehu@gmail.com>
2016-09-18 23:41:18 +0530

    Add checksum

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -7,10 +7,12 @@
 
 
 def Luhn(card_number):
-
+    def digits_of(n):
+        return [int(d) for d in str(n)]
     digits = digits_of(card_number)
     odd = digits[-1::-2]
     even = digits[-2::-2]
-
+    checksum = 0
+    checksum += sum(odd_digits)
 def checksum(card_number):
     return luhn(card_number)
