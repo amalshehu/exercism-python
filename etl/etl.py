6301732d647745c8f8ed44a301139971086cc6f6
Amal Shehu <amalshehu@gmail.com>
2016-09-22 15:44:07 +0530

    Initial commit

---
+++ etl/etl.py
@@ -0,0 +1,5 @@
+#  File:       etl.py
+#  Purpose:    To do the `Transform` step of an Extract-Transform-Load.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Sunday 22 September 2016, 03:40 PM
