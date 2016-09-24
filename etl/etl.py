378f008f63cd9b2dba95d1bc287d1fe12d90c595
Amal Shehu <amalshehu@gmail.com>
2016-09-24 16:10:23 +0530

    Add score table

--- etl/etl.py
+++ etl/etl.py
@@ -2,4 +2,19 @@
 #  Purpose:    To do the `Transform` step of an Extract-Transform-Load.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
-#  Date:       Sunday 22 September 2016, 03:40 PM
+#  Date:       Thursday 22 September 2016, 03:40 PM
+
+
+def transform(num):
+    old = {
+           1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+           2: ["D", "G"],
+           3: ["B", "C", "M", "P"],
+           4: ["F", "H", "V", "W", "Y"],
+           5: ["K"],
+           8: ["J", "X"],
+           10: ["Q", "Z"]
+    }
+    return old[num]
+
+print(transform(1))
