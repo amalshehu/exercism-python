3f79c9aa17a4a6f2214fe307ff8cc9377f0c6e50
Amal Shehu <amalshehu@gmail.com>
2016-09-26 02:01:03 +0530

    Initial commit

---
+++ simple-cipher/simple_cipher.py
@@ -0,0 +1,5 @@
+#  File:       simple_cipher.py
+#  Purpose:    Compute the prime factors of a given natural number.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Monday 26 September 2016, 02:00 AM
