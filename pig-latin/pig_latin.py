d9abaa37f027c3939b2e5b718eb571f418abc079
Amal Shehu <amalshehu@gmail.com>
2016-09-26 01:40:43 +0530

    Initial commit

---
+++ pig-latin/pig_latin.py
@@ -0,0 +1,5 @@
+#  File:       pig_latin.py
+#  Purpose:    Implement a program that translates from English to Pig Latin
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Monday 26 September 2016, 12:40 AM
