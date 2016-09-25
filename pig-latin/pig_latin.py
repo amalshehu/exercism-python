78486b358f8b0d1efec1a05f4e439c659f8ad4f9
Amal Shehu <amalshehu@gmail.com>
2016-09-26 01:54:24 +0530

    Cast translate function

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -3,3 +3,15 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Monday 26 September 2016, 12:40 AM
+
+
+def translate(english):
+    pyg = 'ay'
+    if len(english) > 0 and english.isalpha():
+        word = english.lower()
+        first = word[0]
+        new_word = word[1:len(new_word)] + first + pyg
+        return new_word
+
+    else:
+        return False
