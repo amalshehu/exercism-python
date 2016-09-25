91a589b106b52d083d890eed4a77aec84e8782d3
Amal Shehu <amalshehu@gmail.com>
2016-09-26 00:11:40 +0530

    Initial commit

---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,5 @@
+#  File:       prime_factors.py
+#  Purpose:    Compute the prime factors of a given natural number.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Monday 26 September 2016, 12:05 AM
