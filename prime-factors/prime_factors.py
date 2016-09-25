a4c24372ffcbac656a9879cc2fd705d67a875a3e
Amal Shehu <amalshehu@gmail.com>
2016-09-26 00:21:24 +0530

    Set condition [1 is not a prime]

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -3,3 +3,10 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Monday 26 September 2016, 12:05 AM
+
+
+def prime(number):
+    if number <= 1:
+        return False
+    else:
+        if number % 1 == 0 and number % range(2, number)
