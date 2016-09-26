9b2b3df16e11a704eb1fb547bc95d2d9d06f678b
Amal Shehu <amalshehu@gmail.com>
2016-09-26 17:06:29 +0530

    Generate a key for Cipher if not provided.

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -3,3 +3,16 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Monday 26 September 2016, 02:00 AM
+
+import random
+from string import ascii_lowercase
+
+
+class Cipher():
+    """Generate a key for Cipher if not provided."""
+    def __init__(self, key=None):
+        if not key:
+            key = ''.join(random.SystemRandom().choice(ascii_lowercase) for _ in range(100))
+        elif not key.isalpha() or not key.islower():
+            raise ValueError('Invalid key')
+        self.key = key
