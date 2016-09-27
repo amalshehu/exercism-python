718179202a02b4943d9e93870e43aba562313193
Amal Shehu <amalshehu@gmail.com>
2016-09-27 13:36:58 +0530

    Initial commit

---
+++ scrabble-score/scrabble_score.py
@@ -0,0 +1,5 @@
+#  File:       sscrabble_score.py
+#  Purpose:    Write a program that, given a word, computes the scrabble score for that word.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Tuesday 27 September 2016, 01:27 PM
