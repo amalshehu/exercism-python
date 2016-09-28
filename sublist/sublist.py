ab13fdcada39973902fc9db5644a64c459beabac
Amal Shehu <amalshehu@gmail.com>
2016-09-28 21:41:13 +0530

    Initial commit

---
+++ sublist/sublist.py
@@ -0,0 +1,5 @@
+#  File:       sublist.py
+#  Purpose:    Write a function to determine if a list is a sublist of another list.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Wednesday 28 September 2016, 09:40 PM
