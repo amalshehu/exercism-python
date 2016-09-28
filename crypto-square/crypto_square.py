074e60df105f8719dc86f328285252ee06387a68
Amal Shehu <amalshehu@gmail.com>
2016-09-28 15:31:48 +0530

    Initial commit

---
+++ crypto-square/crypto_square.py
@@ -0,0 +1,5 @@
+#  File:       crypto_square.py
+#  Purpose:    Implement the method for composing secret messages called a square code.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Tuesday 28 September 2016, 03:27 PM
