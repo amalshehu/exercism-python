85a17efa930e058c18fb4c066cf7d40323a74812
Amal Shehu <amalshehu@gmail.com>
2016-09-28 21:35:03 +0530

    Add encode  function

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -3,14 +3,21 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Tuesday 28 September 2016, 03:27 PM
+from math import ceil, sqrt
 
 
 def normalize(message):
-    return ''.join(i for i in message.lower() if i.isalpha())
+    return ''.join([i for i in message if i.isalnum()]).lower()
+
 
 def text_square(text, num):
     if len(text) <= num:
         return [text]
     return [text[:num]] + text_square(text[num:], num)
 
-    
+
+def encode(text):
+    text = normalize(text)
+    side = int(ceil(sqrt(len(text))))
+    clip = text_square(text, side)
+    return ' '.join([''.join(col)for col in izip_longest(*clip, fillvalue='')])
