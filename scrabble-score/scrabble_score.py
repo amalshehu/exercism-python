3058a3f2942f0bf407e54f4ccf799ab6818ac407
Amal Shehu <amalshehu@gmail.com>
2016-09-28 13:16:56 +0530

    Add score table

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -3,3 +3,14 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Tuesday 27 September 2016, 01:27 PM
+
+
+score_table = {
+              1: "A, E, I, O, U, L, N, R, S, T",
+              2: "D, G",
+              3: "B, C, M, P",
+              4: "F, H, V, W, Y",
+              5: "K",
+              8: "J, X",
+              10: "Q, Z"
+}
