266aecd7e1cbca4a9add43fbaa53e0af5c6ab400
Amal Shehu <amalshehu@gmail.com>
2016-09-28 15:18:15 +0530

    Reformat  score table

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,4 +1,4 @@
-#  File:       sscrabble_score.py
+#  File:       scrabble_score.py
 #  Purpose:    Write a program that, given a word, computes the scrabble score for that word.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
@@ -6,11 +6,10 @@
 
 
 score_table = {
-              1: "A, E, I, O, U, L, N, R, S, T",
-              2: "D, G",
-              3: "B, C, M, P",
-              4: "F, H, V, W, Y",
-              5: "K",
-              8: "J, X",
-              10: "Q, Z"
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1,
+    'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8,
+    'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
+    'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
+    'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4,
+    'z': 10
 }
