93acaa39f39e04f736c71780d39601ac6e860268
Amal Shehu <amalshehu@gmail.com>
2016-09-28 16:16:11 +0530

    Normalize message

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -3,3 +3,10 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Tuesday 28 September 2016, 03:27 PM
+
+
+def crypto(message):
+    normalized = ''.join(i for i in message.lower() if i.isalpha())
+    print (normalized)
+
+crypto("If man was meant to stay on the ground, god would have given us roots.")
