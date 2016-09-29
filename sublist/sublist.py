675a70322dfdbd35a317c59f1d6e58c1a7223933
Amal Shehu <amalshehu@gmail.com>
2016-09-29 15:01:49 +0530

    [No issues]convert indent to space / reformat files based on PEP8

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,5 +1,6 @@
 #  File:       sublist.py
-#  Purpose:    Write a function to determine if a list is a sublist of another list.
+#  Purpose:    Write a function to determine if a list is a
+#              sublist of another list.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Wednesday 28 September 2016, 09:40 PM
@@ -9,18 +10,20 @@ SUPERLIST = 1
 EQUAL = 2
 UNEQUAL = 3
 
+
 def check(_list, sub_list):
     for i in range(len(_list) + 1 - len(sub_list)):
         if _list[i:i + len(sub_list)] == sub_list:
             return True
 
+
 def check_lists(_list, sub_list):
     if len(_list) > len(sub_list):
-        if check(_list,sub_list):
+        if check(_list, sub_list):
             return SUPERLIST
 
     if len(sub_list) > len(_list):
-        if check(sub_list,_list):
+        if check(sub_list, _list):
             return SUBLIST
     if len(_list) == len(sub_list):
             return EQUAL if _list == sub_list else UNEQUAL
