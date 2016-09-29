c7f2fabd785ee6e085afc4d9a51a2e700b6fd8f2
Amal Shehu <amalshehu@gmail.com>
2016-09-29 22:35:08 +0530

    Add primitive_triplets function

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -4,7 +4,11 @@
 #  Course:     Exercism
 #  Date:       Thursday 29 September 2016, 04:30 PM
 
+from math import sqrt
 from fractions import gcd
+from itertools import count
+
+
 
 
 def triplets_in_range(min_value, max_value):
@@ -17,7 +21,22 @@ def triplets_in_range(min_value, max_value):
     return triple
 
 
-def is_coprime(a, b):
-    return gcd(a, b) == 1
+def primitive_triplets(b):
+    trips = set()
+    for x, y, z in trip(b):
+        if gcd(x, y) == gcd(y, z) == 1:
+            trips.add((x, y, z))
+    return trips
 
-    
+def trip(b):
+    if b % 4:
+        raise ValueError('Invalid Input')
+    max_num = int(sqrt((b - 1) / 2))
+    for n in range(1, max_num + 1):
+        m, redr = divmod(b / 2, n)
+        if redr == 0:
+            yield(tuple(sorted((m*m - n*n, b, m*m + n*n))))
+
+def is_triplet(triplet):
+    a, b, c = sorted(triplet)
+    return a*a + b*b == c*c
