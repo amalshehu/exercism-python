782e1104fc98a6d406c2e30c23c59bd1f44193e5
Amal Shehu <amalshehu@gmail.com>
2016-09-29 17:13:57 +0530

    Add pythagorean_triples function

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -3,3 +3,17 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 29 September 2016, 04:30 PM
+
+
+def pythagorean_triples(n):
+    n = int(n)
+    if n>0:
+        if a in range(1,n-1):
+            if b in range(a+1, n):
+                if c in range(b+1,n+1):
+                    if a*a+b*b==c*c:
+                        print a,b,c
+    else:
+        return "Wrong input"
+
+pythagorean_triples(30)
