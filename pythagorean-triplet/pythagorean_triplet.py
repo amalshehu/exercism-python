62c86addfab1276b251bf560ed204b10e86d78b0
Amal Shehu <amalshehu@gmail.com>
2016-09-29 16:35:56 +0530

    Initial commit

---
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -0,0 +1,5 @@
+#  File:       pythagorean_triplet.py
+#  Purpose:    A Pythagorean triplet program
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Thursday 29 September 2016, 04:30 PM
