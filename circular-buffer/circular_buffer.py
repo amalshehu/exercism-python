7ceb421df7bd2d850d3d9f9490b4c9e47ed72d36
Amal Shehu <amalshehu@gmail.com>
2016-09-29 23:18:58 +0530

    Add CircularBuffer function

--- circular-buffer/circular_buffer.py
+++ circular-buffer/circular_buffer.py
@@ -4,3 +4,11 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Thursday 29 September 2016, 10:48 PM
+
+import collections
+
+
+def CircularBuffer(value):
+    d = collections.deque(maxlen=7)
+    for i in xrange(10):
+        d.append(i)
