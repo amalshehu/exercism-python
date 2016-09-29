e867097b85ff9b591cb2e70b30771299f4aa72f5
Amal Shehu <amalshehu@gmail.com>
2016-09-29 13:59:50 +0530

    Add check_lists function

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -3,3 +3,19 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Wednesday 28 September 2016, 09:40 PM
+UNEQUAL = 0
+EQUAL = 1
+SUBLIST = 3
+SUPERLIST = 4
+
+
+def check_lists(_list, sub_list):
+    for item in sub_list:
+        if sub_list.count(item) == _list.count(item):
+            return EQUAL
+        elif sub_list.count(item) < _list.count(item):
+            return SUBLIST
+        elif sub_list.count(item) > _list.count(item):
+            return SUPERLIST
+        elif sub_list.count(item) != _list.count(item):
+            return UNEQUAL
