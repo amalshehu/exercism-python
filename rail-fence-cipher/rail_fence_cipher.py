33c5c56c442f2f0c50bc93022193de4bb50a6504
Amal Shehu <amalshehu@gmail.com>
2016-09-30 20:35:32 +0530

    Initial commit

---
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -0,0 +1,5 @@
+#  File:       rail_fence_cipher.py
+#  Purpose:    Implement encoding and decoding for the rail fence cipher.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Friday 30 September 2016, 08:35 PM
