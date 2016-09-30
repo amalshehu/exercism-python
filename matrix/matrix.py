94d2f6ce7c02a4b4dbd910df1beb0e9b748d82da
Amal Shehu <amalshehu@gmail.com>
2016-09-30 19:30:48 +0530

    Add matrix_row  function

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -4,3 +4,17 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Friday 30 September 2016, 07:11 PM
+import re
+
+
+class Matrix():
+
+    """Matrix row and column."""
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def matrix_row(self, matrix):
+        return re.findall(r'\d+', matrix)
+
+mat = Matrix("12254646")
+print(mat.matrix_row())
