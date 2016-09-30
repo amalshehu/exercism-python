20b1505a316fc47049470f998741f00569df818f
Amal Shehu <amalshehu@gmail.com>
2016-09-30 21:51:50 +0530

    Add imaginary fence list comprehension

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -3,3 +3,17 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Friday 30 September 2016, 08:35 PM
+
+
+class Rail():
+    """Implementation of Rail Fence Cipher."""
+    def __init__(self, text, rail_count):
+        self.text = text
+        self.rail_count = rail_count
+
+    def imaginary_fence(self):
+        fence = [[None] * len(self.text) for num in range(self.rail_count)]
+        print (fence)
+
+f = Rail('helloworld', 3)
+f.imaginary_fence()
