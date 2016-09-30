203c9e708180164b4ec211409b96fb9a73f3ca21
Amal Shehu <amalshehu@gmail.com>
2016-09-30 23:44:02 +0530

    Add encode and decode functions

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -5,12 +5,11 @@
 #  Date:       Friday 30 September 2016, 08:35 PM
 
 
-class Rail():
+class RailFence():
     """Implementation of Rail Fence Cipher."""
     def __init__(self, text, rail_count):
         self.text = text
         self.rail_count = rail_count
-
     def imaginary_fence(self):
         fence = [[None] * len(self.text) for num in range(self.rail_count)]
         rails = range(self.rail_count - 1) + range(self.rail_count - 1, 0, -1)
@@ -19,8 +18,9 @@ class Rail():
 
         return [i for rail in fence for i in rail if i is not None]
 
-        print (rails)
+    def encode(self):
+        return ''.join(imaginary_fence(self.text, self.rail_count))
 
-
-f = Rail('helloworld', 3)
-f.imaginary_fence()
+    def decode(self):
+        dec = imaginary_fence(range(len(self.text)), self.rail_count)
+        return ''.join(self.text[dec.index(i)] for i in range(len(self.text)))
