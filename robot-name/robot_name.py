5e8192fcee17110b8fd417986dc6bef612658125
Amal Shehu <amalshehu@gmail.com>
2016-09-30 15:01:55 +0530

    Initial commit

---
+++ robot-name/robot_name.py
@@ -0,0 +1,5 @@
+#  File:       robot_name.py
+#  Purpose:    Write a program that manages robot factory settings.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Friday 30 September 2016, 03:00 PM
