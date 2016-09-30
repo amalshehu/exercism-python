41156ebce44d97bbc1aa42ae572b0100322764c3
Amal Shehu <amalshehu@gmail.com>
2016-10-01 00:28:29 +0530

    Fix TypeError

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -10,7 +10,7 @@ class RailFence():
     def __init__(self, text, rail_count):
         self.text = text
         self.rail_count = rail_count
-    def imaginary_fence(self):
+    def imaginary_fence(self, text, rail_count):
         fence = [[None] * len(self.text) for num in range(self.rail_count)]
         rails = range(self.rail_count - 1) + range(self.rail_count - 1, 0, -1)
         for n, letter in enumerate(self.text):
@@ -19,8 +19,13 @@ class RailFence():
         return [i for rail in fence for i in rail if i is not None]
 
     def encode(self):
-        return ''.join(imaginary_fence(self.text, self.rail_count))
+        return ''.join(self.imaginary_fence(self.text, self.rail_count))
 
     def decode(self):
-        dec = imaginary_fence(range(len(self.text)), self.rail_count)
-        return ''.join(self.text[dec.index(i)] for i in range(len(self.text)))
+        r = range(len(self.text))
+        dec = self.imaginary_fence(r, self.rail_count)
+        return ''.join(self.text[dec.index(i)] for i in r)
+
+
+rail = RailFence('WECRLTEERDSOEEFEAOCAIVDEN', 3)
+print (rail.encode())
