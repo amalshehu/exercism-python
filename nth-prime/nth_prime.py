70ed14240f5792c14fab22abef2276a13049130d
Amal Shehu <amalshehu@gmail.com>
2016-09-30 23:46:23 +0530

    Initial commit

---
+++ nth-prime/nth_prime.py
@@ -0,0 +1,5 @@
+#  File:       nth_prime.py
+#  Purpose:    Write a program that can tell you what the nth prime is.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Friday 30 September 2016, 11:50 PM
