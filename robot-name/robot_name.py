1886f80366248149d3d74020a54b78fdcbc826f4
Amal Shehu <amalshehu@gmail.com>
2016-09-30 19:04:06 +0530

    Add reset function

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -11,14 +11,16 @@ import random
 class Robot():
     """Robot facory settings"""
     def __init__(self):
-        self.name = ""
+        self.name = self.factory_name()
 
     def factory_name(self):
-
         char = ''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(2))
         num = ''.join(random.SystemRandom().choice(string.digits) for _ in range(3))
         self.name = char + num
         return self.name
+
+    def reset(self):
+        self.name = self.factory_name()
 
 R1 = Robot()
 print(R1.factory_name())
