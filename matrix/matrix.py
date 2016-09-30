b51e683fcdaf2df6bfb7ce33b2657599445c6467
Amal Shehu <amalshehu@gmail.com>
2016-09-30 19:11:54 +0530

    Initial commit

---
+++ matrix/matrix.py
@@ -0,0 +1,6 @@
+#  File:       matrix.py
+#  Purpose:    Write a program that, given a string representing a matrix
+#              of numbers, can return the rows and columns of that matrix.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Friday 30 September 2016, 07:11 PM
