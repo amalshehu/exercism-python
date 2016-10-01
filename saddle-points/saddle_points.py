c6fde553f61f8b9dddb6dfc9af067f8f2489c8ea
Amal Shehu <amalshehu@gmail.com>
2016-10-01 21:44:24 +0530

    Initial commit

---
+++ saddle-points/saddle_points.py
@@ -0,0 +1,5 @@
+#  File:       saddle_points.py
+#  Purpose:    Write a program that detects saddle points in a matrix.
+#  Programmer: Amal Shehu
+#  Course:     Exercism
+#  Date:       Saturday 1 September 2016, 09:40 PM
