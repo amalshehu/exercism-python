5d68e9b5d10a188736bfeb186b9183c68a8f581d
Amal Shehu <amalshehu@gmail.com>
2016-10-02 01:34:47 +0530

    Initial commit

---
+++ beer-song/beer_song.py
@@ -0,0 +1,6 @@
+#  File       :	beer_song.py
+#  Purpose    :	Write a program which produces the lyrics to that beloved
+#               classic, that field-trip : 99 Bottles of Beer on the Wall.
+#  Programmer : Amal Shehu
+#  Course     :	Exercism
+#  Date       :	Sunday 2 October 2016, 01:33 AM
