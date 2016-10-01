5499897f494bad5f32250364d710db23ee71f04e
Amal Shehu <amalshehu@gmail.com>
2016-10-02 02:08:05 +0530

    Add beer_song function skeleton

--- beer-song/beer_song.py
+++ beer-song/beer_song.py
@@ -4,3 +4,20 @@
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Sunday 2 October 2016, 01:33 AM
+
+
+def beer_song():
+    for beer_count in range(99, 0, -1):
+        if beer_count > 1:
+            print beer_count, "bottles of beer on the wall,", beer_count, "bottles of beer."
+            if beer_count > 2:
+                suffix = str(beer_count - 1) + " bottles of beer on the wall."
+            else:
+                suffix = "1 bottle of beer on the wall."
+        elif beer_count == 1:
+            print "1 bottle of beer on the wall, 1 bottle of beer."
+            suffix = "no more beer on the wall!"
+            print "Take one down, pass it around,", suffix
+            print "--"
+
+beer_song()
