be94f03e10d8b48d22dd366dfe947e56eec701a3
Amal Shehu <amalshehu@gmail.com>
2016-10-02 00:34:35 +0530

    Add SaddlePoints class

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -2,4 +2,16 @@
 #  Purpose:    Write a program that detects saddle points in a matrix.
 #  Programmer: Amal Shehu
 #  Course:     Exercism
-#  Date:       Saturday 1 September 2016, 09:40 PM
+#  Date:       Saturday 1 October 2016, 09:40 PM
+
+
+class SaddlePoints():
+
+    """Matrix saddle points."""
+    def __init__(self, matrix):
+        self.matrix = self.saddle_points(matrix)
+
+    def saddle_points(matrix):
+    SP = []
+
+    return (SP)
