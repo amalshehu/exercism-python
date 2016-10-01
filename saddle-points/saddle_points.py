83d52bd63aadb669126ac1e006d822e119860858
Amal Shehu <amalshehu@gmail.com>
2016-10-02 01:28:36 +0530

    Reformat program

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,17 +1,18 @@
-#  File:       saddle_points.py
-#  Purpose:    Write a program that detects saddle points in a matrix.
+#  File:	   saddle_points.py
+#  Purpose:	Write a program that detects saddle points in a matrix.
 #  Programmer: Amal Shehu
-#  Course:     Exercism
-#  Date:       Saturday 1 October 2016, 09:40 PM
+#  Course:	 Exercism
+#  Date:	   Saturday 1 October 2016, 09:40 PM
 
-
-class SaddlePoints():
-
-    """Matrix saddle points."""
-    def __init__(self, matrix):
-        self.matrix = self.saddle_points(matrix)
-
-    def saddle_points(matrix):
-    SP = []
-
-    return (SP)
+def saddle_points(matrix):
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise ValueError("Invalid Matrix Dimension")
+    rows = matrix
+    cols = zip(*matrix)
+    point = set()
+    for c in range(len(cols)):
+        for r in range(len(rows)):
+            saddle = matrix[r][c]
+            if saddle == max(rows[r]) and saddle == min(cols[c]):
+                point.add((r, c))
+    return point
