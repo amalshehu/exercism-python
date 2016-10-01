2728e5dc488efa781a5b4d4d51544301cdc1a802
Amal Shehu <amalshehu@gmail.com>
2016-10-01 20:12:33 +0530

    Add nth_prime function

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -3,3 +3,16 @@
 #  Programmer: Amal Shehu
 #  Course:     Exercism
 #  Date:       Friday 30 September 2016, 11:50 PM
+
+
+def nth_prime(limit):
+
+    for num in range(limit):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                print num
+
+print nth_prime(15)
