d71eec9ba6bbb81a44f0040168160e6c375b3008
Amal Shehu <amalshehu@gmail.com>
2016-10-02 21:05:38 +0530

    Initial commit

---
+++ perfect-numbers/perfect_numbers.py
@@ -0,0 +1,6 @@
+#  File       :	beer_song.py
+#  Purpose    :	The Greek mathematician Nicomachus devised a
+#               classification scheme for natural numbers.
+#  Programmer : Amal Shehu
+#  Course     :	Exercism
+#  Date       :	Sunday 2 October 2016, 09:04 PM
