05608e4c920efb39909d9c856c69b3e12df4002a
Amal Shehu <amalshehu@gmail.com>
2016-10-02 23:55:14 +0530

    Add factors function

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,6 +1,16 @@
-#  File       :	beer_song.py
+#  File       :	perfect_numbers.py
 #  Purpose    :	The Greek mathematician Nicomachus devised a
 #               classification scheme for natural numbers.
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Sunday 2 October 2016, 09:04 PM
+
+
+def factors(num):
+    factor = []
+    for n in range(1, num):
+        if num % n == 0:
+            factor.append(n)
+    return factor
+
+print factors(5)
