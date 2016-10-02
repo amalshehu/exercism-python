6a20cd7e3b0f83b8c08b5939d5cff044e4d0cb99
Amal Shehu <amalshehu@gmail.com>
2016-10-02 21:02:17 +0530

    Reformat functions

--- beer-song/beer_song.py
+++ beer-song/beer_song.py
@@ -6,20 +6,25 @@
 #  Date       :	Sunday 2 October 2016, 01:33 AM
 
 
-def beer_song():
-    for beer_count in range(99, 0, -1):
-        if beer_count > 1:
-            print beer_count, "bottles of beer on the wall,", beer_count, "bottles of beer."
-            current_count = beer_count - 1
-            print "Take one down, pass it around,", current_count, "bottles of beer on the wall."
-            if beer_count > 2:
-                suffix = str(beer_count - 1) + " bottles of beer on the wall."
-            else:
-                suffix = "1 bottle of beer on the wall."
-        elif beer_count == 1:
-            print "1 bottle of beer on the wall, 1 bottle of beer."
-            suffix = "no more beer on the wall!"
-            print "Take one down, pass it around,", suffix
-            print "--"
+def song():
+    for beer_count in range(99, -1, -1):
+        verse(beer_count)
 
-beer_song()
+
+def verse(beer_count):
+    if beer_count > 1:
+        print beer_count, "bottles of beer on the wall,", beer_count, "bottles of beer.\n"
+        current_count = beer_count - 1
+        print "Take one down and pass it around,", current_count, "bottles of beer on the wall.\n"
+        if beer_count > 2:
+            suffix = str(beer_count - 1) + " bottles of beer on the wall.\n"
+        else:
+            suffix = "1 bottle of beer on the wall.\n"
+    elif beer_count == 1:
+        print "1 bottle of beer on the wall, 1 bottle of beer.\n"
+        suffix = "no more beer on the wall!\n"
+        print "Take one down, pass it around,", suffix
+
+    elif beer_count == 0:
+        print "No more bottles of beer on the wall, no more bottles of beer.\n"
+        print "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
