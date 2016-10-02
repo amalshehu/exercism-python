9d74cea93004308dbb1248e18a571fac9f3cd549
Amal Shehu <amalshehu@gmail.com>
2016-10-03 00:01:45 +0530

    Add conditions

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -4,13 +4,19 @@
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Sunday 2 October 2016, 09:04 PM
+from numpy import prod
 
 
-def factors(num):
+def is_perfect(num):
     factor = []
     for n in range(1, num):
         if num % n == 0:
             factor.append(n)
-    return factor
-
-print factors(5)
+    fact_sum = (sum(factor))
+    fact_pro = prod(factor)
+    if fact_pro == fact_sum:
+        return True
+    elif fact_pro > fact_sum:
+        return False
+    elif fact_pro < fact_sum:
+        return False
