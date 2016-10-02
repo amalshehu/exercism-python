b7cb8c5306508f0c1db2c50479f7f764e7e8bb0e
Amal Shehu <amalshehu@gmail.com>
2016-10-03 00:51:48 +0530

    Initial commit

---
+++ secret-handshake/handshake.py
@@ -0,0 +1,6 @@
+#  File       :	handshake.py
+#  Purpose    :	Write a program that will take a decimal number, and
+#               convert it to the sequence of events for a secret handshake.
+#  Programmer : Amal Shehu
+#  Course     :	Exercism
+#  Date       :	Monday 3 October 2016, 12:50 AM
