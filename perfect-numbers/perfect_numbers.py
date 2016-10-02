d943b9116de4bfa5425fa6320545183783209b9e
Amal Shehu <amalshehu@gmail.com>
2016-10-03 00:45:15 +0530

    List comprehension

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -4,19 +4,16 @@
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Sunday 2 October 2016, 09:04 PM
-from numpy import prod
+
+Abundant = False
+Deficient = False
 
 
 def is_perfect(num):
-    factor = []
-    for n in range(1, num):
-        if num % n == 0:
-            factor.append(n)
-    fact_sum = (sum(factor))
-    fact_pro = prod(factor)
-    if fact_pro == fact_sum:
+    factor = sum([n for n in range(1, num+1) if n % num == 0])
+    if num == fact_sum:
         return True
-    elif fact_pro > fact_sum:
-        return False
-    elif fact_pro < fact_sum:
-        return False
+    elif num > fact_sum:
+        return Deficient
+    elif num < fact_sum:
+        return Abundant
