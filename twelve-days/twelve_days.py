c4366e64e21ff94f929ceec204a5e2783a145e53
Amal Shehu <amalshehu@gmail.com>
2016-10-08 18:46:32 +0530

    Initial commit

---
+++ twelve-days/twelve_days.py
@@ -0,0 +1,5 @@
+#  File       :	secret_handshake.py
+#  Purpose    :	Write a program that outputs the lyrics to 'The Twelve Days '
+#  Programmer : Amal Shehu
+#  Course     :	Exercism
+#  Date       :	Saturday 8 October 2016, 06:45 PM
