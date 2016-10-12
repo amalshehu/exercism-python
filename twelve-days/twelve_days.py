268c98476ae21929c9834d86742391d6f25c31d6
Amal Shehu <amalshehu@gmail.com>
2016-10-12 14:59:10 +0530

    [No issues]convert indent to space / reformat files based on PEP8

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -5,8 +5,8 @@
 #  Date       :	Saturday 8 October 2016, 06:45 PM
 import inflect
 gift = ["Drummers Drumming", 'Pipers Piping', 'Lords-a-Leaping',
-        'adies Dancing', 'Maids-a-Milking','Swans-a-Swimming',
-        'Geese-a-Laying','Gold Rings', 'Calling Birds','French Hens',
+        'Ladies Dancing', 'Maids-a-Milking', 'Swans-a-Swimming',
+        'Geese-a-Laying', 'Gold Rings', 'Calling Birds', 'French Hens',
         'Turtle Doves', 'a Partridge in a Pear Tree.\n']
 gift_key = [x for x in range(1, 13)]
 gifts = dict(zip(gift_key, reversed(gift)))
@@ -15,19 +15,22 @@ gifts = dict(zip(gift_key, reversed(gift)))
 def sing():
     return verses(1, 12)
 
+
 def verse(v):
     p = inflect.engine()
     day = p.number_to_words(v)
     day_count = p.ordinal(day)
     if v == 1:
-        lyric = "On the %s day of Christmas my true love gave to me, " % (day_count)
+        lyric = "On the %s day of Christmas my true love gave" \
+                "to me, " % (day_count)
         lyric = lyric + gifts[v]
         return lyric
     if v > 1:
-        lyric = "On the %s day of Christmas my true love gave to me, " % (day_count)
-        for k in range(v,1,-1):
+        lyric = "On the %s day of Christmas my true love gave" \
+                "to me, " % (day_count)
+        for k in range(v, 1, -1):
             num = p.number_to_words(k)
-            lyric = lyric + num +" "+ gifts[k] + ", "
+            lyric = lyric + num + " " + gifts[k] + ", "
         lyric += "and " + gifts[1]
         return lyric
 
@@ -38,11 +41,4 @@ def verses(v1, v2):
         lyric += verse(i) + '\n'
     return lyric
 
-print ("On the fifth day of Christmas my true love gave to me, "
-            "five Gold Rings, "
-            "four Calling Birds, "
-            "three French Hens, "
-            "two Turtle Doves, "
-            "and a Partridge in a Pear Tree.\n")
-
-print verse(5)
+print (sing())
