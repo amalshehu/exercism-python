7f20739894f954d87cd16228e5811f0601e46ee6
Amal Shehu <amalshehu@gmail.com>
2016-10-12 15:03:52 +0530

    Initial commit

---
+++ binary/binary.py
@@ -0,0 +1,5 @@
+#  File       :	binary.py
+#  Purpose    :	Write a program that will convert a binary number, represented as a string (e.g. '101010'), to its decimal equivalent using first principles
+#  Programmer : Amal Shehu
+#  Course     :	Exercism
+#  Date       :	Saturday 12 October 2016, 03:02 PM
