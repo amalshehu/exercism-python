34db1d0b9b9505adc8b12f9a36eca8a33db1bb2d
Amal Shehu <amalshehu@gmail.com>
2016-10-12 14:51:24 +0530

    Update functions

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -6,28 +6,43 @@
 import inflect
 gift = ["Drummers Drumming", 'Pipers Piping', 'Lords-a-Leaping',
         'adies Dancing', 'Maids-a-Milking','Swans-a-Swimming',
-        'Geese-a-Laying','Gold Rings', 'Calling Birds',' French Hens',
+        'Geese-a-Laying','Gold Rings', 'Calling Birds','French Hens',
         'Turtle Doves', 'a Partridge in a Pear Tree.\n']
 gift_key = [x for x in range(1, 13)]
 gifts = dict(zip(gift_key, reversed(gift)))
 
-def sing():
-    pass
 
+def sing():
+    return verses(1, 12)
 
 def verse(v):
     p = inflect.engine()
     day = p.number_to_words(v)
     day_count = p.ordinal(day)
     if v == 1:
-        lyric = "On the %s day of Christmas my true love gave to me," % (day_count)
+        lyric = "On the %s day of Christmas my true love gave to me, " % (day_count)
         lyric = lyric + gifts[v]
         return lyric
     if v > 1:
-        lyric = "On the %s day of Christmas my true love gave to me," % (day_count)
-        lyric = lyric + gifts[v]
+        lyric = "On the %s day of Christmas my true love gave to me, " % (day_count)
+        for k in range(v,1,-1):
+            num = p.number_to_words(k)
+            lyric = lyric + num +" "+ gifts[k] + ", "
+        lyric += "and " + gifts[1]
         return lyric
-def verses(v1, v2):
-    pass
 
-print verse(2)
+
+def verses(v1, v2):
+    lyric = ''
+    for i in range(v1, v2+1):
+        lyric += verse(i) + '\n'
+    return lyric
+
+print ("On the fifth day of Christmas my true love gave to me, "
+            "five Gold Rings, "
+            "four Calling Birds, "
+            "three French Hens, "
+            "two Turtle Doves, "
+            "and a Partridge in a Pear Tree.\n")
+
+print verse(5)
