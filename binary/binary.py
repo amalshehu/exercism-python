f2561d414403d365243a3cd868ca82eba5e606f9
Amal Shehu <amalshehu@gmail.com>
2016-10-13 18:54:12 +0530

    Add binary function

--- binary/binary.py
+++ binary/binary.py
@@ -3,3 +3,13 @@
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Saturday 12 October 2016, 03:02 PM
+
+def binary(bin_value):
+    sum_ = 0
+    num = list(bin_value)
+    l = len(bin_value - 1)
+    for i in num:
+        sum_ += int(i) * (2 ** l)
+    return sum_
+
+print binary('11011')
