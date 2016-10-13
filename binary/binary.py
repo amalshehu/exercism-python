a824eedf624f2ff089ae9f0ecf160a0dc0426170
Amal Shehu <amalshehu@gmail.com>
2016-10-13 20:14:03 +0530

    Add regex

--- binary/binary.py
+++ binary/binary.py
@@ -3,9 +3,15 @@
 #  Programmer : Amal Shehu
 #  Course     :	Exercism
 #  Date       :	Saturday 12 October 2016, 03:02 PM
-
+import re
 def parse_binary(bin_value):
     sum_ = 0
     for i in bin_value:
+        matches = re.match('[01]*$',bin_value)
+        if not matches:
+            print i
+            raise ValueError('Not a valid binary number.')
         sum_ = sum_ * 2 + int(i)
     return sum_
+
+print parse_binary('1011')
